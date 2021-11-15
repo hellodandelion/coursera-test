c58daad4bac4dfa85e901c611531d60276e013c6
Daniel Morrison <daniel@danieljmorrison.com>
2021-11-14 22:32:25 -0600

    adding ej files

--- ej/js/script.js
+++ ej/js/script.js
@@ -1,8 +1,27 @@
-let number = 0
-while (number <= 7) {
-	console.log(number);
-	number = number + 1;
-}
+// let number = 0
+// while (number <= 100) {
+// 	console.log(number);
+// 	number = number + 1;
+// 		if (number % 3) {}
+// }
 
-for (let triangle = "#"; triangle.length < 8; triangle += "#")
-  console.log(triangle);
+// for (let number = 0; number <= 100; number +=1) {
+// 	console.log(number);
+// 	if(number % 3) {
+// 		console.log("Fizz")
+// 	}
+// }
+
+
+
+// for (var number=1; number < 101; number++){
+//     if (number % 15 == 0) console.log("FizzBuzz");
+//     else if (number % 3 == 0) console.log("Fizz");
+//     else if (number % 5 == 0) console.log("Buzz");
+//     else console.log(number);
+// }
+
+// for (let triangle = "#"; triangle.length < 8; triangle += "#")
+//   console.log(triangle);
+
+
