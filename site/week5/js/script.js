36310100542f0f590408b5ff00826731fa16edd6
Daniel Morrison <daniel@danieljmorrison.com>
2021-11-23 19:30:18 -0600

    adding index.html and js

---
+++ site/week5/js/script.js
@@ -0,0 +1,31 @@
+// DOM manipulation
+// console.log(document.getElementById("title"));
+// console.log(document instanceof HTMLDocument);
+
+console.log(document.getElementById("title"));
+
+
+// function sayHello () {
+//   var name =
+//    document.getElementById("name").value;
+//    var message = "<h2>Hello " + name + "!</h2>";
+
+//   // document
+//   //   .getElementById("content")
+//   //   .textContent = message;
+
+//   document
+//     .getElementById("content")
+//     .innerHTML = message;
+
+//   if (name === "student") {
+//     var title = 
+//       document
+//         .querySelector("#title")
+//         .textContent;
+//     title += " & Lovin' it!";
+//     document
+//         .querySelector("h1")
+//         .textContent = title;
+//   }
+// }
